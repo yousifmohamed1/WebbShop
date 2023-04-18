8cd44aa77c5b97920299a460d6393856e3f0c3e3
Yousif <yousifmohamed51@outlook.com>
2023-04-18 16:09:23 +0200

    First Commit

--- backend/demo/src/main/java/com/example/demo/Controllers/Hej.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.Controllers;
-
-public class Hej {
-    public static void main(String[] args) {
-        System.out.println("hej då");
-    }
-}
--- backend/demo/src/main/java/com/example/demo/Repositorys/Hej.java
+++
@@ -1,4 +0,0 @@
-package com.example.demo.Repositorys;
-
-public interface Hej {
-}
